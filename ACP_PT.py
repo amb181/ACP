44ab4494217aed9a8ec48bce9084122aee749aa4
Alejandro Molina <44465097+amb181@users.noreply.github.com>
2019-11-18 20:43:32 -0600

    Versión validada con MATLAB
    Se realizaron pruebas con MATLAB y se obtuvieron los mismos resultados de valores y vectores propios, así como la gráfica de los puntos.

--- ACP_PT.py
+++ ACP_PT.py
@@ -12,9 +12,8 @@ import csv
 #Carga de datos
 #Abrir el archivo .csv 
 data = r'C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\ACP.csv'
-df = pd.read_csv(data, names = ['Aceites y grasas','Alimentos de origen animal',
-                                'Azucares','Bebidas','Cereales','Comida mexicana',
-                                'Frutas','Leches','Legumbres','Verduras','ID'])
+df = pd.read_csv(data)
+col = list(df.columns.values)
 
 n = len(df.columns)                      #Contar columnas
 m = len(list(csv.reader(open(data))))    #Contar filas
@@ -34,43 +33,45 @@ mat_cov = np.cov(df_.T)
 #Calculo vectores propios y valores propios
 eigenvalores, eigenvectores = np.linalg.eig(mat_cov)
 
+#Calculo del porcentaje de aportacion de cada componente
+CP1 = str(round(eigenvalores[0]*100/sum(eigenvalores),2))
+CP2 = str(round(eigenvalores[1]*100/sum(eigenvalores),2))
+
 #Hacemos una lista por parejas (eigenvalor, eigenvector) 
-eig_par = [(np.abs(eigenvalores[i]), eigenvectores[:,i]) for i in range(len(eigenvalores))]
+eig_par = [(np.abs(eigenvalores[i]), eigenvectores[:,i], col[i]) for i in range(len(eigenvalores))]
 
 # Orden de las tuplas (eigenvalor, eigenvector) de mayor a menor
 eig_par.sort(key=lambda x: x[0], reverse=True)
 
-## Lista de eigenvalores en orden descendiente y formacion del FeatureVector
-#for i in eig_par:
-#    print(i[0])
+# Lista de eigenvalores en orden descendiente y formacion del FeatureVector
+for i in eig_par:
+    print(i[0])
 #    print(i[1])
-
+#    print(i[2])
+    
 #A partir de los valores propios, calculamos la varianza explicada
 tot = sum(eigenvalores)
-var_exp = [(i / tot)*100 for i in sorted(eigenvalores, reverse=True)]
+var_exp = [(i / tot)*100 for i in (eigenvalores)]
 sum_var_exp = np.cumsum(var_exp)
 
-#Representamos en un diagrama de barras la varianza por cada eigenvalor, y la acumulada
+#Representamos en un diagrama de barras cada eigenvalor y la varianza acumulada
 with plt.style.context('seaborn-pastel'):
     fig, ax1 = plt.subplots()
     ax1.set_ylabel('Valor propio')
     ax1.set_xlabel('Componentes Principales')
-    ax1.bar(range(n-1), eigenvalores/10, alpha=0.5, align='center',
-            label='Contribucion del componente', color='g')
-    ax1.tick_params(axis='y')
+    ax1.bar(range(n-1), eigenvalores, alpha=0.5, align='center', color='g')
+    plt.xticks(range(n-1), col, fontsize=6, rotation=45)
     #Instancia un segundo eje que comparte al eje x
     ax2 = ax1.twinx()
     ax2.plot(range(n-1), sum_var_exp, 'o-', label='Varianza acumulada (%)')
-    ax2.set_ylabel('Porcentaje de Varianza Explicada')
-    ax2.set_xlabel('Componentes Principales')
-    ax2.tick_params(axis='y')
+    ax2.set_ylabel('Porcentaje de Varianza')
     plt.savefig('CP.jpg')
     plt.legend(loc='best')
     plt.tight_layout()
 
-    
 #Creamos el FeatureVector = [eig1, eig2, ...]
 fv = np.hstack((eig_par[0][1].reshape(n-1, 1), eig_par[1][1].reshape(n-1, 1)))
+fv = fv*-1
 
 #Calculo de los datos finales, FinalData = RowFeatureVector x RowDataAdjust
 rfv = np.transpose(fv)
@@ -88,8 +89,7 @@ axx.scatter(fd[0], fd[1], color='red')
 for i, txt in enumerate(y):
     axx.annotate(txt, (fd[0][i], fd[1][i]))
 plt.grid()
-plt.ylabel('CP2')
-plt.xlabel('CP1')
+plt.ylabel('CP2' + '('+CP2+'%)')
+plt.xlabel('CP1' + '('+CP1+'%)')
 fig2.savefig('PCA.jpg')
-
  
